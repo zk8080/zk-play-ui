ad3f63bb1b1eee9b79f1d271c8d843f2f9dc2539
张康 <zhangkang8080@163.com>
2021-08-23 22:36:18 +0800

    test: Input组件单元测试用例

---
+++ src/Input/__test__/input.test.tsx
@@ -0,0 +1,54 @@
+/*
+ * @Author: your name
+ * @Date: 2021-08-23 22:04:47
+ * @LastEditTime: 2021-08-23 22:35:52
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /zk-play-ui/src/Input/__test__/input.test.tsx
+ */
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { Input, InputProps } from '../input';
+
+const testState = { test: '' };
+
+const defaultProps: InputProps = {
+  onChange: (e) => {
+    testState.test = e.target.value;
+  },
+  placeholder: 'test-input',
+};
+describe('test Input component', () => {
+  it('should render the correct default Input', () => {
+    const wrapper = shallow(<Input {...defaultProps} />);
+    expect(wrapper.find('input').hasClass('viking-input-inner')).toBeTruthy();
+    wrapper.find('input').simulate('change', { target: { value: '23' } });
+    expect(testState.test).toEqual('23');
+  });
+  it('should render the disabled Input on disabled property', () => {
+    const wrapper = shallow(<Input disabled placeholder="disabled" />);
+    expect(
+      wrapper.find('.viking-input-wrapper').hasClass('is-disabled'),
+    ).toBeTruthy();
+  });
+  it('should render different input sizes on size property', () => {
+    const wrapper = shallow(<Input placeholder="sizes" size="lg" />);
+    expect(
+      wrapper.find('.viking-input-wrapper').hasClass('input-size-lg'),
+    ).toBeTruthy();
+  });
+  it('should render prepand and append element on prepand/append property', () => {
+    const wrapper = mount(
+      <Input placeholder="pend" prepend="https://" append=".com" />,
+    );
+    expect(
+      wrapper
+        .find('.viking-input-wrapper')
+        .hasClass('input-group input-group-append input-group-prepend'),
+    ).toBeTruthy();
+    expect(wrapper.find('.viking-input-group-prepend').text()).toEqual(
+      'https://',
+    );
+    expect(wrapper.find('.viking-input-group-append').text()).toEqual('.com');
+  });
+});
