63959b3d08ccd58ce97cf4ae7e4f280e495301e3
张康 <zhangkang8080@163.com>
2021-08-29 21:52:10 +0800

    feat: autoComplete组件支持动态模版和异步加载数据

--- src/AutoComplete/autoComplete.tsx
+++ src/AutoComplete/autoComplete.tsx
@@ -1,46 +1,76 @@
 /*
  * @Author: your name
  * @Date: 2021-08-26 22:34:12
- * @LastEditTime: 2021-08-26 23:17:32
+ * @LastEditTime: 2021-08-29 21:46:06
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /zk-play-ui/src/AutoComplete/index.tsx
  */
-import React, { FC, useState, ChangeEvent } from 'react';
+import React, { FC, useState, ChangeEvent, ReactElement } from 'react';
 import Input, { InputProps } from '../Input/input';
 import classNames from 'classnames';
+import Icon from '../Icon/index';
+interface DataSourceObject {
+  value: string;
+}
 
+export type DataSourceType<T = {}> = T & DataSourceObject;
 export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
-  fetchSuggestions: (str: string) => string[];
-  onSelect?: (str: string) => void;
+  fetchSuggestions: (
+    str: string,
+  ) => DataSourceType[] | Promise<DataSourceType[]>;
+  onSelect?: (item: DataSourceType) => void;
+  renderOption?: (item: DataSourceType) => ReactElement;
 }
 
 const prefixCls = 'zk-play-autoComplete';
 
 const AutoComplete: FC<AutoCompleteProps> = (props) => {
-  const { fetchSuggestions, onSelect, className, value, ...resetProps } = props;
+  const {
+    fetchSuggestions,
+    onSelect,
+    className,
+    value,
+    renderOption,
+    ...resetProps
+  } = props;
 
   const Cls = classNames(prefixCls, className);
   // 输入框数据
   const [inputVal, setInputVal] = useState<string>(value as string);
   // 要显示的列表数据
-  const [suggestions, setSuggestions] = useState<string[]>([]);
+  const [suggestions, setSuggestions] = useState<DataSourceType[]>([]);
+  // 是否显示loading
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const val = e.target.value.trim();
     setInputVal(val);
     if (val) {
       const result = fetchSuggestions(val);
-      setSuggestions(result);
+      if (result instanceof Promise) {
+        setLoading(true);
+        result.then((data) => {
+          setLoading(false);
+          setSuggestions(data);
+        });
+      } else {
+        setSuggestions(result);
+      }
     } else {
       setSuggestions([]);
     }
   };
 
-  const handleSelect = (item: string) => {
-    setInputVal(item);
+  const handleSelect = (item: DataSourceType) => {
+    setInputVal(item.value);
     setSuggestions([]);
     onSelect?.(item);
+  };
+
+  // 动态渲染模版
+  const renderTemplate = (item: DataSourceType) => {
+    return renderOption ? renderOption(item) : item.value;
   };
 
   // 渲染显示列表
@@ -50,7 +80,7 @@ const AutoComplete: FC<AutoCompleteProps> = (props) => {
         {suggestions.map((item, index) => {
           return (
             <li key={index} onClick={() => handleSelect(item)}>
-              {item}
+              {renderTemplate(item)}
             </li>
           );
         })}
@@ -61,6 +91,11 @@ const AutoComplete: FC<AutoCompleteProps> = (props) => {
   return (
     <div className={Cls}>
       <Input value={inputVal} onChange={handleChange} {...resetProps} />
+      {loading && (
+        <ul>
+          <Icon icon="spinner" spin></Icon>
+        </ul>
+      )}
       {suggestions.length > 0 && generateDropdown()}
     </div>
   );
--- src/AutoComplete/index.md
+++ src/AutoComplete/index.md
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-08-26 23:04:57
- * @LastEditTime: 2021-08-26 23:18:11
+ * @LastEditTime: 2021-08-29 21:40:40
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /zk-play-ui/src/AutoComplete/index.md
@@ -14,18 +14,93 @@
 ```tsx
 import React from 'react';
 import { AutoComplete } from 'zk-play-ui';
+import type { DataSourceType } from 'zk-play-ui/es/AutoComplete/autoComplete.d';
 
-const testData = ['apple', 'orange', 'green', 'yellow', 'red', 'blue'];
+interface LakerPlayerProps {
+  value: string;
+  number: number;
+}
+// interface GithubUserProps {
+//   login: string;
+//   url: string;
+//   avatar_url: string;
+// }
+
+const testData = [
+  { value: 'bradley', number: 11 },
+  { value: 'pope', number: 1 },
+  { value: 'caruso', number: 4 },
+  { value: 'cook', number: 2 },
+  { value: 'cousins', number: 15 },
+  { value: 'james', number: 23 },
+  { value: 'AD', number: 3 },
+  { value: 'green', number: 14 },
+  { value: 'howard', number: 39 },
+  { value: 'kuzma', number: 0 },
+];
 
 export default () => {
   const handleFetch = (val) => {
-    return testData.filter((item) => item.includes(val));
+    return testData.filter((item) => item.value.includes(val));
   };
   const handleSelect = (val) => {
     console.log(val);
   };
+  const renderOption = (item: DataSourceType) => {
+    const lakerPlayerItem = item as DataSourceType<LakerPlayerProps>;
+    return (
+      <>
+        <h2>Name: {lakerPlayerItem.value}</h2>
+        <p>Number: {lakerPlayerItem.number}</p>
+      </>
+    );
+  };
   return (
-    <AutoComplete fetchSuggestions={handleFetch} onSelect={handleSelect} />
+    <AutoComplete
+      fetchSuggestions={handleFetch}
+      onSelect={handleSelect}
+      renderOption={renderOption}
+    />
+  );
+};
+```
+
+## 异步获取数据
+
+```tsx
+import React from 'react';
+import { AutoComplete } from 'zk-play-ui';
+import type { DataSourceType } from 'zk-play-ui/es/AutoComplete/autoComplete.d';
+
+interface GithubUserProps {
+  login: string;
+  url: string;
+  avatar_url: string;
+}
+
+export default () => {
+  const handleFetch = (query: string) => {
+    return fetch(`https://api.github.com/search/users?q=${query}`)
+      .then((res) => res.json())
+      .then(({ items }) => {
+        console.log(items);
+        return items
+          .slice(0, 10)
+          .map((item: any) => ({ value: item.login, ...item }));
+      });
+  };
+
+  const renderOption = (item: DataSourceType) => {
+    const itemWithGithub = item as DataSourceType<GithubUserProps>;
+    return (
+      <>
+        <h2>Name: {itemWithGithub.value}</h2>
+        <p>url: {itemWithGithub.url}</p>
+      </>
+    );
+  };
+  return (
+    <AutoComplete fetchSuggestions={handleFetch} renderOption={renderOption} />
   );
 };
 ```
