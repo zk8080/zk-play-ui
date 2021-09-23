068acbb17b2f26bf4a927e20e59b662b5d80cb7a
张康 <zhangkang8080@163.com>
2021-09-23 22:21:34 +0800

    build: 修改build配置

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,4 +1,27 @@
+// export default {
+//   esm: 'babel',
+//   cjs: 'babel',
+// };
+
+// esm -> es module 浏览器环境
+// cjs -> nodejs 环境（测试|ssr）
+// umd -> 通过浏览器 script 引入的方式
+// 默认 esm: 'rollup' 打包在一起，无法实现按需加载
+
 export default {
-  esm: 'babel',
+  entry: 'src/index.ts',
+  esm: {
+    type: 'babel',
+    minify: false,
+    importLibToEs: true,
+  },
   cjs: 'babel',
+  umd: {
+    file: 'zk-play-ui',
+    minFile: true,
+    sourcemap: true,
+  },
+  lessInBabelMode: false,
+  extractCSS: true,
+  runtimeHelpers: true,
 };
